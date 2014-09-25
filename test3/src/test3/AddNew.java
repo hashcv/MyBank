26e7a7fc6ef2b1dbc9dfa24264d492f939d053ec
Hashcv <hash.cv@gmail.com>
2014-09-25 17:47:19 +0300

    343

--- test3/src/test3/AddNew.java
+++ test3/src/test3/AddNew.java
@@ -18,32 +18,36 @@ public class AddNew extends HttpServlet {
 	private static Storage storage = Storage.getInstance();
 	private static List<Customer> custs = storage.getCusts();
 	private static List<Account> accs = storage.getAccs();
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AddNew() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String firstName=request.getParameter("firstName");
-		String lastName=request.getParameter("lastName"); 
-		String ipn=request.getParameter("ipn"); 
-		String address=request.getParameter("address"); 
-		String phone=request.getParameter("phone"); 
-		String email=request.getParameter("email"); 
-		
-		custs.add(new Customer(firstName, lastName, Long.parseLong(ipn), address, phone, email ));
-		
+	public AddNew() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String ipn = request.getParameter("ipn");
+		String address = request.getParameter("address");
+		String phone = request.getParameter("phone");
+		String email = request.getParameter("email");
+
+		custs.add(new Customer(firstName, lastName, Long.parseLong(ipn),
+				address, phone, email));
+
 		request.setAttribute("list", custs);
-		request.getRequestDispatcher("/servletWelcome.jsp").forward(
-				request, response);
-		
+		request.getRequestDispatcher("/servletWelcome.jsp").forward(request,
+				response);
+
 	}
 
 }
